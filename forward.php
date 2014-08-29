6c2720ebba1b54cb9c6485521175189211f9dff0
Christopher Berkompas <chris@leadsimple.net>
2014-08-29 11:43:28 -0700

    Finished renaming files

---
+++ forward.php
@@ -0,0 +1,16 @@
+<?php
+error_reporting(E_ALL);
+require('require_all.php');
+$site_domain = 'http://' . $_SERVER["SERVER_NAME"];
+
+$call = new InboundCall($_POST);
+header ("Content-Type:text/xml");
+
+?><?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Dial timeout="45" action="<?php echo $site_domain . '/track.php'; ?>">
+    <Number>
+      <?php echo $call->destination_number; ?>
+    </Number>
+  </Dial>
+</Response>
