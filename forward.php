ba0a81a96eca0a462801bce57d3b4d03ceefa74c
Christopher Berkompas <chris@leadsimple.net>
2014-08-29 11:57:58 -0700

    Tweak

--- forward.php
+++ forward.php
@@ -4,7 +4,7 @@ require('require_all.php');
 $site_domain = 'http://' . $_SERVER["SERVER_NAME"];
 
 $call = new InboundCall($_POST);
-header ("Content-Type: text/xml");
+//header ("Content-Type: text/xml");
 
 ?><?xml version="1.0" encoding="UTF-8"?>
 <Response>
