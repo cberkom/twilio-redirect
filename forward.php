426ec84d803da82f6f88ce037a77c81c55e467e6
Christopher Berkompas <chris@leadsimple.net>
2014-08-29 12:14:06 -0700

    Attempted xml fix

--- forward.php
+++ forward.php
@@ -5,7 +5,7 @@ $site_domain = 'http://' . $_SERVER["SERVER_NAME"];
 
 $call = new InboundCall($_POST);
 header ("Content-Type: text/xml");
-
+echo '<?xml version="1.0" encoding="utf-8" ?>'
 ?>
 <Response>
   <Dial timeout="45" action="<?php echo $site_domain . '/track.php'; ?>">
