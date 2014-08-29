7349bc65f496f10393b4b26255669a6347d5abe9
Christopher Berkompas <chris@leadsimple.net>
2014-08-29 12:58:36 -0700

    Fix how urls are constructed

--- forward.php
+++ forward.php
@@ -1,14 +1,13 @@
 <?php
 error_reporting(0);
 require('require_all.php');
-$site_domain = 'http://' . $_SERVER["SERVER_NAME"];
 
 $call = new InboundCall($_POST);
 header ("Content-Type: text/xml");
 echo '<?xml version="1.0" encoding="utf-8" ?>'
 ?>
 <Response>
-  <Dial timeout="45" action="<?php echo $site_domain . '/track.php'; ?>">
+  <Dial timeout="45" action="<?php echo url_to('/track.php'); ?>">
     <Number>
       <?php echo $call->destination_number; ?>
     </Number>
