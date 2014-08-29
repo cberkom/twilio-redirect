9fa2d4b7e90544e1e899182b3dd781ccfca6ee09
Christopher Berkompas <chris@leadsimple.net>
2014-08-29 12:08:26 -0700

    Tweak

--- forward.php
+++ forward.php
@@ -1,16 +1,16 @@
 <?php
-//error_reporting(E_ALL);
-//require('require_all.php');
-//$site_domain = 'http://' . $_SERVER["SERVER_NAME"];
+error_reporting(0);
+require('require_all.php');
+$site_domain = 'http://' . $_SERVER["SERVER_NAME"];
 
-//$call = new InboundCall($_POST);
-//header ("Content-Type: text/xml");
+$call = new InboundCall($_POST);
+header ("Content-Type: text/xml");
 
-?>Hello
+?>
 <Response>
   <Dial timeout="45" action="<?php echo $site_domain . '/track.php'; ?>">
     <Number>
-      <?php echo "World"; //$call->destination_number; ?>
+      <?php echo $call->destination_number; ?>
     </Number>
   </Dial>
 </Response>
