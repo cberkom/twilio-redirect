fcc84b998008764cb6de9ace729adbf30d918652
Christopher Berkompas <chris@leadsimple.net>
2014-08-29 12:06:10 -0700

    Tweak

--- forward.php
+++ forward.php
@@ -6,11 +6,11 @@
 //$call = new InboundCall($_POST);
 //header ("Content-Type: text/xml");
 
-?><?xml version="1.0" encoding="UTF-8"?>
+?>Hello
 <Response>
   <Dial timeout="45" action="<?php echo $site_domain . '/track.php'; ?>">
     <Number>
-      <?php echo "Hello World"; //$call->destination_number; ?>
+      <?php echo "World"; //$call->destination_number; ?>
     </Number>
   </Dial>
 </Response>
